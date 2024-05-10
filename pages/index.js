fedd4037dc19d2989a46d26fb0a90846ba6a1a29
yinghuzhu <yinghuzhu@gmail.com>
2024-05-10 10:21:56 +0800

    new vercel

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Welcome to Your Company's Homepage</h1>
+      <p>This is the landing page for your company.</p>
+    </div>
+  );
+}
